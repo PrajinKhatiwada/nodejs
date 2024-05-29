9a1ae0e41e08772c3f14712cadcf8f0348f02ac1
Prajin Khatiwada <prajinkhatiwada@gmail.com>
2024-05-29 12:22:53 +0545

    ECMA modules

---
+++ ecma.js
@@ -0,0 +1,4 @@
+import * as a2 from "./ecmamodule.mjs"
+
+console.log(a2.prajin())
+console.log(a2.srijey())
---
+++ ecmamodule.mjs
@@ -0,0 +1,7 @@
+export  function prajin(){
+    console.log("ecma modules")
+}
+
+export function srijey(){
+    console.log(" hey how are you ?")
+}
