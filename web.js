4ac3fee15ca31f7af8d13a9b35e1d55148df85ef
Prajin Khatiwada <prajinkhatiwada@gmail.com>
2024-05-30 00:22:21 +0545

    static file server created

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 
 const http = require('http');
+const fs=require('fs');
 const port =process.env.PORT|| 3000;
 
 const server =http.createServer((req, res)=>{
@@ -9,10 +10,14 @@ const server =http.createServer((req, res)=>{
     res.setHeader('Content-Type','text/html')
     console.log(req.url)
     if(req.url=='/'){
-        res.end('<h1>HeLLO its me Prajin Khatiwada</h1><br><p> A passionate mernstack developer from Nepal.</p>');
+        res.statusCode=200;
+        const data=fs.readFileSync('index.html');
+        res.end(data.toString());
+        
     }
     else if(req.url=='/about') {
-        res.end('<h1>About Me </h1><br><p> A passionate mernstack developer from Nepal.</p>');
+        res.statusCode=200;
+        res.end('<h1>HeLLO its me Prajin Khatiwada</h1><br><p> A passionate mernstack developer from Nepal.</p>');
     }
    else{
     res.statusCode=404;
